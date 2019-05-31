80c4f6bc917ba6015d0b7d05881bf6c257089fd0
Linyheart <linyheart@linyheart.com>
2019-05-31 17:40:59 +0800

    update all fisrt version

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: https://www.pineal.cn/
+// | @author_url 2: https://www.pineal.cn/
+// | @author_email: 18581281315@163.com   
+// +------------------------------------------------------------------------+
+// | ShongGuo - The Ultimate Video Sharing Platform
+// | Copyright (c) 2018 ShongGuo. All rights reserved.
+// +------------------------------------------------------------------------+
+// 数据库地址
+$sql_db_host = "localhost";
+// 数据库用户
+$sql_db_user = "root";
+// 数据库密码
+$sql_db_pass = "linyheart";
+// 数据库名称
+$sql_db_name = "linyheart";
+
+// 网站 URL
+$site_url = "http://127.0.0.1/baYiSchoolMicrolecturePlatform/"; // 比如 (https://www.pineal.cn)
+
+$purchase_code = "777777"; // 
+?>
